564be5c2136ce0605d454c8853c3a5b85a3ca5bd
jcahal <jacahal@gmail.com>
2023-06-03 13:57:21 -0700

    sos button implimented

--- lib/controller.dart
+++ lib/controller.dart
@@ -3,73 +3,92 @@ import 'dart:async';
 import 'package:torch_light/torch_light.dart';
 import 'package:flashie/model.dart';
 
+enum TimerMode { sos, inf }
+
 class Controller {
   Controller();
 
-  static final Model _state = Model();
-  static final Future<bool> torchAvailble = TorchLight.isTorchAvailable();
-  static final StreamController<Model> _streamController = StreamController<Model>.broadcast();
-  
+  static final _state = Model();
+  static final torchAvailble = TorchLight.isTorchAvailable();
+  static final _streamController = StreamController<Model>.broadcast();
+  static Timer? _periodicTimer;
+
   static Stream<Model> get stream => _streamController.stream;
   static Model get state => _state;
 
-  Timer _timer;
-
-  static void toggleTorch() {
-    _state.isTorchEnabled ? disableTorch() : enableTorch();
-  }
-  
-  static void enableTorch() async {
+  static void _enableTorch() async {
     // await TorchLight.enableTorch();
     _state.setTorchEnabled = true;
     _streamController.sink.add(_state);
   }
 
-  static void disableTorch() async {
+  static void _disableTorch() async {
     // await TorchLight.disableTorch();
     _state.setTorchEnabled = false;
     _streamController.sink.add(_state);
   }
 
+  static void _toggleTorch() {
+    _state.isTorchEnabled ? _disableTorch() : _enableTorch();
+  }
+
+  static void _toggleTorchPeriodically(int millis, TimerMode mode) {
+    _periodicTimer = Timer.periodic(Duration(milliseconds: millis), (timer) {
+      _toggleTorch();
+
+      if (mode == TimerMode.sos) {
+        if (timer.tick == 6) {
+          timer.cancel();
+          _toggleTorchPeriodically(millis == 500 ? 1000 : 500, TimerMode.sos);
+        }
+      }
+    });
+  }
+
   static void onToggleButtonPressed() {
-    toggleTorch();
+    _toggleTorch();
   }
 
   static void onIntervalSliderChanged(double value) {
     _state.setIntervalSliderValue = value;
-    // _torchIntervalTimer?.cancel();
-    // _onTorchIntervalButtonPressed(_torchIntervalButtonEnabled);
+    if (_state.isIntervalButtonEnabled) {
+      _periodicTimer?.cancel();
+      _toggleTorchPeriodically(
+          (_state.currentIntervalSliderValue * 1000).round(), TimerMode.inf);
+    }
 
     _streamController.sink.add(_state);
   }
 
   static void onIntervalButtonPressed() async {
-    _state.isIntervalButtonEnabled 
-    ? _state.setIntervalButtonEnabled = false 
-    : _state.setIntervalButtonEnabled = true;
-
-    // if (_torchIntervalButtonEnabled) {
-    //   _torchIntervalTimer = Timer.periodic(
-    //     Duration(milliseconds: (_torchIntervalSiderValue * 1000.00).round()),
-    //     (timer) => _torchState.toggleTorch(),
-    //   );
-    // } else {
-    //   _torchIntervalTimer?.cancel();
-    // }
+    if (_state.isIntervalButtonEnabled) {
+      _periodicTimer?.cancel();
+      _disableTorch();
+      _state.setIntervalButtonEnabled = false;
+    } else {
+      _periodicTimer?.cancel();
+      _disableTorch();
+      _toggleTorchPeriodically(
+          (_state.currentIntervalSliderValue * 1000).round(), TimerMode.inf);
+      _state.setIntervalButtonEnabled = true;
+    }
 
     _streamController.sink.add(_state);
   }
 
   static void onSOSButtonPressed() async {
-    _state.isSOSButtonEnabled 
-    ? _state.setSOSButtonEnabled = false 
-    : _state.setSOSButtonEnabled = true;
-     
-    // SOS Logic
-    Timer.periodic(Duration(milliseconds: 500), (timer) {
-        
-     });
-     
+    if (_state.isSOSButtonEnabled) {
+      _periodicTimer?.cancel();
+      _disableTorch();
+      _state.setSOSButtonEnabled = false;
+    } else {
+      _periodicTimer?.cancel();
+      _disableTorch();
+      _state.setIntervalButtonEnabled = false;
+      _toggleTorchPeriodically(500, TimerMode.sos);
+      _state.setSOSButtonEnabled = true;
+    }
+
     _streamController.sink.add(_state);
   }
 }
--- lib/model.dart
+++ lib/model.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 class Model {
   Model();
 
@@ -6,12 +8,14 @@ class Model {
   bool _isToggleButtonEnabled = false;
   bool _isIntervalButtonEnabled = false;
   double _currentIntervalSliderValue = 1.0;
+  // Timer _timer = Timer();
 
   bool get isTorchEnabled => _isTorchEnabled;
   bool get isSOSButtonEnabled => _isSOSButtonEnabled;
   bool get isToggleButtonEnabled => _isToggleButtonEnabled;
   bool get isIntervalButtonEnabled => _isIntervalButtonEnabled;
   double get currentIntervalSliderValue => _currentIntervalSliderValue;
+  // Timer get timer => _timer;
 
   set setTorchEnabled(bool value) {
     _isTorchEnabled = value;
@@ -32,4 +36,8 @@ class Model {
   set setIntervalSliderValue(double value) {
     _currentIntervalSliderValue = value;
   }
+
+  // set setTimer(Timer value) {
+  //   _timer = value;
+  // }
 }
